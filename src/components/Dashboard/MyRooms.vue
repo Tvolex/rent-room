82fcdcbb4d3eca255f4b977940fc48af33f7b3a3
tisyanchin <vasil.tisyanchin@foxtrapp.com>
2019-02-09 02:42:49 +0200

    feat(myRooms): markup title, icon, hovers, loader

--- src/components/Dashboard/MyRooms.vue
+++ src/components/Dashboard/MyRooms.vue
@@ -1,41 +1,55 @@
 <template>
     <v-layout row wrap class="MyRooms">
-        <v-flex xs12 sm6 md4
+        <v-flex xs12 sm6 md4 lg3
+                v-if="rooms"
                 v-for="(room, i) in rooms"
                 :key="i"
         >
-            <v-card hover class="elevation-5 card-room" height="250" width="300">
+            <v-card hover class="elevation-5 card-room" height="250">
                 <v-img
                         :src="room.photos && room.photos.length && room.photos[0].location ? room.photos[0].location.fit : '@/assets/no-photo.png'"
                         height="250"
-                        width="300"
                 >
-                    <v-card-title>
-                        <div class="grey white--text"> <h3>{{room.title}}</h3></div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn flat color="white" class="" @click="openRoom(room)">Open</v-btn>
-                    </v-card-actions>
+
+                    <v-card-text>
+                        <div class="text-xs-right white--text">
+                            <v-icon dark>visibility</v-icon>
+                            {{room.views}}
+                        </div>
+                    </v-card-text>
+                   <div class="info-about-room">
+                       <v-card-text class="title-room">
+                           <div class="headline white--text"> {{room.title}}</div>
+                           <div class="white--text">{{room.createdBy.date | parseDate}}</div>
+                       </v-card-text>
+                   </div>
                 </v-img>
             </v-card>
         </v-flex>
+        <ShadowCard v-if="loading" :count="5"></ShadowCard>
         <v-flex xs12 sm6 md4>
-            <v-card  class="elevation-0 car-add-new-room" color="transparent" height="250" width="300">
-                <v-card-title primary-title>
-                    <span>Add new</span>
-                    <div class="button-add-new">
-                        <v-icon class="button-add-new-title">add</v-icon>
+            <v-card class="card-add-new-room" color="transparent" height="250">
+                <v-card-text>
+                    <div class="button-add-new ">
+                        <v-icon class="icon-add-new">add</v-icon>
                     </div>
-                </v-card-title>
-
+                </v-card-text>
+                <v-card-text class="text-xs-center title-new-ad " align="center">
+                    <p>Here you can create a new ad.</p>
+                </v-card-text>
             </v-card>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
+    import moment from 'moment'
+    import ShadowCard from './ShadowCard';
     export default {
         name: "MyRooms",
+        components: {
+            ShadowCard,
+        },
         beforeMount() {
             this.getMyRooms();
         },
@@ -49,11 +63,13 @@
                 search: null,
                 sort: { by: "price", order: -1 },
                 filter: {},
+                loading: false,
             }
         },
 
         methods: {
             getMyRooms: function () {
+                this.loading = true;
                 this.$store.dispatch({type: 'getMyRooms',
                     filter: this.filter,
                     search: this.search,
@@ -65,29 +81,69 @@
                     this.total = data.total;
                 })
                     .catch(err => this.errorHandler(err, { notify: true }))
+                    .finally(() => {
+                        this.loading = false;
+                    })
             }
         },
+        filters: {
+            parseDate: function (value) {
+                console.log(moment().locale());
+                const date = new moment(value);
+                date.locale('uk');
+                return date.format('LLLL');
+            },
+        }
     }
 </script>
 
 <style scoped>
+    .MyRooms {
+        min-height: 100vh;
+    }
+
     .card-room {
         margin: 0 0 20px 15px;
         border-radius: 10px;
     }
 
-    .car-add-new-room {
-        border: 1px dashed grey !important;
+    .info-about-room {
+        width: 100%;
+        background-color: rgba(0,0,0, 0.2);
+    }
+
+     .title-room {
+         margin-top: 120px;
+     }
+
+    .icon-add-new {
+        margin-top: 13px;
+        margin-left: 13px;
+    }
+
+    .card-add-new-room {
+        margin: 0 0 20px 15px;
+        border: 2px dashed #b3b3b3  !important;
         -webkit-box-shadow: none !important;
         box-shadow: none !important;
+        cursor: pointer;
+    }
+
+    .card-add-new-room:hover {
+        border: 2px dashed grey !important;
+    }
+
+    .title-new-ad {
+        color: grey;
+        height: 50px !important;
     }
 
     .button-add-new {
-        background: rgba(0,0,0, 0.5);
+        background: rgba(0,0,0, 0.25);
         height: 50px;
         width: 50px;
-        margin-top: 100px;
-        margin-right: 150px;
+        margin-top: 75px;
+        margin-left: 100px;
         cursor: pointer;
         border-radius: 50%;
         padding: 0 auto;
---
+++ src/components/Dashboard/ShadowCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-layout row wrap>
+        <v-flex xs12 sm6 md4
+                v-for="shadow in count"
+                :key="shadow">
+            <v-card hover class="card-shadow" >
+                <vue-content-loading :width="300" :height="250">
+                    <rect x="0" y="0" rx="10" ry="10" width="350" height="200" />
+                    <rect x="10" y="220" rx="10" ry="10" width="250" height="20" />
+                    <rect x="10" y="250" rx="10" ry="10" width="150" height="15" />
+
+                    <rect x="10" y="300" rx="10" ry="10" width="80" height="35" />
+                    <rect x="150" y="300" rx="10" ry="10" width="130" height="35" />
+                </vue-content-loading>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    // TODO: format shadow card
+    import VueContentLoading from 'vue-content-loading'
+    export default {
+        name: "ShadowCard",
+        components: {
+            VueContentLoading,
+        },
+        props: {
+            count: {
+                type: Number,
+                default: 5,
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .card-shadow {
+        margin: 0 0 20px 15px;
+        border-radius: 10px;
+    }
+</style>
