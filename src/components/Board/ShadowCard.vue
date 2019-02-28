b96d28b251dba4a982d9dd24ffcb5affefacf351
tisyanchin <vasil.tisyanchin@foxtrapp.com>
2019-02-28 12:49:46 +0200

    fix(shadowCard): remove v-hover

--- src/components/Board/ShadowCard.vue
+++ src/components/Board/ShadowCard.vue
@@ -3,18 +3,16 @@
         <v-flex xs12 sm6 md4
                 v-for="shadow in count"
                 :key="shadow">
-            <v-hover>
-                <v-card class="card-shadow" slot-scope="{ hover }" >
-                    <vue-content-loading :width="350" :height="350">
-                        <rect x="0" y="0" rx="10" ry="10" width="350" height="200" />
-                        <rect x="10" y="220" rx="10" ry="10" width="250" height="20" />
-                        <rect x="10" y="250" rx="10" ry="10" width="150" height="15" />
+            <v-card class="card-shadow">
+                <vue-content-loading :width="350" :height="350">
+                    <rect x="0" y="0" rx="10" ry="10" width="350" height="200" />
+                    <rect x="10" y="220" rx="10" ry="10" width="250" height="20" />
+                    <rect x="10" y="250" rx="10" ry="10" width="150" height="15" />
 
-                        <rect x="10" y="300" rx="10" ry="10" width="80" height="35" />
-                        <rect x="150" y="300" rx="10" ry="10" width="130" height="35" />
-                    </vue-content-loading>
-                </v-card>
-            </v-hover>
+                    <rect x="10" y="300" rx="10" ry="10" width="80" height="35" />
+                    <rect x="150" y="300" rx="10" ry="10" width="130" height="35" />
+                </vue-content-loading>
+            </v-card>
         </v-flex>
     </v-layout>
 </template>
